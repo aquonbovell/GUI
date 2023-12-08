b42a741ec1246697491792518d0655d6dc94fdd7
Aquon Bovell <aquon_bovell@outlook.com>
2023-12-07 20:44:09 -0400

    Refactor GUI code in App.java

--- src/App.java
+++ src/App.java
@@ -5,21 +5,21 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class App {
-	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
 
-		JFrame frame = new JFrame("JFrame Example");
-		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout());
-		JLabel label = new JLabel("JFrame By Example");
-		JButton button = new JButton();
-		button.setText("Button");
-		panel.add(label);
-		panel.add(button);
-		frame.add(panel);
-		frame.setSize(200, 300);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}
+        JFrame frame = new JFrame("JFrame Example");
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JLabel label = new JLabel("JFrame By Example");
+        JButton button = new JButton();
+        button.setText("Button");
+        panel.add(label);
+        panel.add(button);
+        frame.add(panel);
+        frame.setSize(200, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
